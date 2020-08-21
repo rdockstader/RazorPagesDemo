c442aeb1ff26d6766d7aede78515cbcfac546e93
Ryan Dockstader <ryanldockstader@gmail.com>
2020-08-21 10:04:02 -0600

    Add project files.

---
+++ RazorPagesUI/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorPagesUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        [BindProperty(SupportsGet = true)]
+        public string City { get; set; }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrEmpty(City))
+            {
+                City = "The Web";
+            }
+        }
+    }
+}
